a5588aec813cf54c83516ec0af439f631afa2f5a
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-10-02 00:13:31 +0530

    added route for animal messages

--- src/routes/careTakerRoutes.js
+++ src/routes/careTakerRoutes.js
@@ -2,6 +2,7 @@ const express=require('express')
 const auth=require('../middleware/auth.js')
 const careTakerController=require("../controller/careTakerController.js")
 const animalController=require("../controller/animalController.js")
+const animalMessagesController=require("../controller/animalMessagesController.js")
 const careTakerActionController=require("../controller/careTakerActionController.js")
 const router=new express.Router()
 
@@ -11,5 +12,7 @@ router.post("/createAnimals",auth,animalController.create);
 router.post("/getAnimal",auth,animalController.get);
 
 router.post("/getMessages",auth,careTakerActionController.get);
+router.post("/createMessage",auth,animalMessagesController.create);
+router.post("/getAnimalMessages",auth,animalMessagesController.get);
 
 module.exports=router
